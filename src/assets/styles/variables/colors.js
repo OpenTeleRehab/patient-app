55a9edfe2a0cd29d7fe4df17f4e477bd22fe35cc
Lina <lina@web-essentials.co>
2021-03-02 09:32:28 +0700

    [TASK] Implement goad card
    refs TRA-317

--- src/assets/styles/variables/colors.js
+++ src/assets/styles/variables/colors.js
@@ -8,6 +8,7 @@ export default {
   blueDark: '#06038D',
   blueLight: '#3392D3',
   blueLight2: '#0065BF',
+  blueLight3: '#B3D6EF',
   orangeDark: '#c95508',
   orangeLight: '#EDC8A3',
   red: '#FF3B2F',
