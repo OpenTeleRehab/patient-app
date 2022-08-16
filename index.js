7d751d289194151a50ff8dfb107020220038df1c
Phearoth Maksay <phearoth@web-essentials.co>
2022-08-16 09:30:49 +0700

    [BUGFIX] Correct receiving call on Android
    refs TRA-754

--- index.js
+++ index.js
@@ -3,11 +3,11 @@
  */
 
 import React from 'react';
-import {AppRegistry, Platform} from 'react-native';
+import {AppRegistry, Platform, PermissionsAndroid} from 'react-native';
 import App from './App';
 import {name as appName} from './app.json';
 import messaging from '@react-native-firebase/messaging';
-import RNCallKeep from 'react-native-callkeep';
+import RNCallKeep from '@webessentials/react-native-callkeep';
 import uuid from 'react-native-uuid';
 import {getLocalData, storeLocalData} from './src/utils/local_storage';
 import {STORAGE_KEY} from './src/variables/constants';
@@ -23,14 +23,6 @@ messaging().setBackgroundMessageHandler(async (remoteMessage) => {
       RNCallKeep.endCall(callInfo.callUUID);
       await storeLocalData(STORAGE_KEY.CALL_INFO, {}, true);
     } else {
-      RNCallKeep.displayIncomingCall(
-        callUUID,
-        remoteMessage.data.body,
-        remoteMessage.data.title,
-        'generic',
-        remoteMessage.data.body.includes('video'),
-      );
-
       const callInfo = {
         callUUID,
         rid: remoteMessage.data.rid,
@@ -39,6 +31,74 @@ messaging().setBackgroundMessageHandler(async (remoteMessage) => {
       };
 
       await storeLocalData(STORAGE_KEY.CALL_INFO, callInfo, true);
+
+      if (Platform.OS === 'ios') {
+        RNCallKeep.displayIncomingCall(
+          callUUID,
+          remoteMessage.data.body,
+          remoteMessage.data.title,
+          'generic',
+          remoteMessage.data.body.includes('video'),
+        );
+      } else {
+        const options = {
+          ios: {
+            appName: 'PatientApp',
+            imageName: 'sim_icon',
+            supportsVideo: true,
+            maximumCallGroups: '1',
+            maximumCallsPerCallGroup: '1',
+          },
+          android: {
+            alertTitle: 'Permissions required',
+            alertDescription:
+              'This application needs to access your phone accounts',
+            cancelButton: 'Cancel',
+            okButton: 'ok',
+            additionalPermissions: [
+              PermissionsAndroid.PERMISSIONS.READ_PHONE_STATE,
+              PermissionsAndroid.PERMISSIONS.READ_PHONE_NUMBERS,
+            ],
+            foregroundService: {
+              channelId: 'org.hi.patient',
+              channelName: 'Foreground service for my app',
+              notificationTitle: 'My app is running on background',
+              notificationIcon: 'Path to the resource icon of the notification',
+            },
+          },
+        };
+
+        RNCallKeep.checkPhoneAccountEnabled()
+          .then((test) => {
+            let isOnCall = false;
+            RNCallKeep.registerPhoneAccount(options);
+            RNCallKeep.registerAndroidEvents();
+            RNCallKeep.setAvailable(true);
+            RNCallKeep.displayIncomingCall(
+              callUUID,
+              remoteMessage.data.body,
+              remoteMessage.data.title,
+              'generic',
+              remoteMessage.data.body.includes('video'),
+            );
+            RNCallKeep.addEventListener('answerCall', async () => {
+              isOnCall = true;
+              await storeLocalData(STORAGE_KEY.REJECTED_CALL, 'false', false);
+              RNCallKeep.backToForeground();
+              RNCallKeep.endCall(callUUID);
+            });
+
+            RNCallKeep.addEventListener('endCall', async () => {
+              if (isOnCall !== true) {
+                await storeLocalData(STORAGE_KEY.REJECTED_CALL, 'true', false);
+                RNCallKeep.backToForeground();
+              }
+            });
+          })
+          .catch((e) => {
+            console.log('Error while initializing call keep: ', e);
+          });
+      }
     }
 
     console.log(
