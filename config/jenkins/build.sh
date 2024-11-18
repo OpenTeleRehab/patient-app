f2e4803d50a462537cf19677b4cfde170c4ca604
Ratha Heang <ratha@web-essentials.co>
2024-11-18 09:17:19 +0700

    [TASK] Add deployment latest develop branch
    refs SS-00

--- config/jenkins/build.sh
+++ config/jenkins/build.sh
@@ -1,3 +1,10 @@
 #!/bin/bash -ex
 
-exit 0
+FILE_NAME=$(basename "$0")
+STAGE="${FILE_NAME%.*}"
+
+ansible-playbook config/ansible/playbook.yml -vv \
+    -e patch_release_version=${BUILD_ID} \
+    -e build_id=${BUILD_ID} \
+    -e workspace=${WORKSPACE} \
+    -e jenkin_home=$JENKINS_HOME
